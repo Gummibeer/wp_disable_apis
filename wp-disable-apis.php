f673377635214c7587a7e7adcbb1f289c1b4d7d6
Tom Witkowski <dev.gummibeer@gmail.com>
2017-02-10 17:12:08 +0100

    Update wp-disable-apis.php

--- wp-disable-apis.php
+++ wp-disable-apis.php
@@ -14,6 +14,7 @@ add_filter('json_enabled', '__return_false');
 add_filter('json_jsonp_enabled', '__return_false');
 add_filter('rest_enabled', '__return_false');
 add_filter('rest_jsonp_enabled', '__return_false');
+add_filter( 'rest_authentication_errors','throw_rest_auth_error');
 
 remove_action('wp_head', 'rsd_link');
 remove_action('xmlrpc_rsd_apis', 'rest_output_rsd');
@@ -21,9 +22,13 @@ remove_action('wp_head', 'rest_output_link_wp_head', 10);
 remove_action('template_redirect', 'rest_output_link_header', 11);
 
 if (defined('XMLRPC_REQUEST') && XMLRPC_REQUEST) {
-    exit;
+    wp_die();
 }
 
 if (defined('REST_REQUEST') && REST_REQUEST) {
-    exit;
+    wp_die();
+}
+
+function throw_rest_auth_error( $access ) {
+        return new WP_Error( 'api_disabled', 'The REST API is disabled.', [ 'status' => 404 ] );
 }
